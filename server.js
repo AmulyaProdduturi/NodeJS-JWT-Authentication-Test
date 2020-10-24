07232a58c2c64faf7bf2a34d97fb085202433708
Amulya Prodduturi <amulyareddyp15@gmail.com>
2020-10-23 21:42:31 -0400

    Added token expiration

--- server.js
+++ server.js
@@ -1,44 +1,30 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
 
 const jwt = require('jsonwebtoken');
-
-const exjwt = require('express-jwt');
-var axios = require('axios');
-
+const exjwt = require('express-jwt')
 const bodyParser = require('body-parser');
 
 const path = require('path');
-app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin', 'http:/localhost:3000');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-type, Authorization');
-    next();
-});
-
+var axios = require('axios');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(bodyParser.urlencoded({extended : true}));
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Headers','Content-type,Authorization');
+    next();
+})
 
 const PORT = 3000;
 
-const secretKey= "My super secret key";
-
+const secretKey = 'My super secret key';
 const jwtMW = exjwt({
-    secret: secretKey,
-    algorithms: ['HS256'],
-    isRevoked: isRevokedCallback
-});
+    secret : secretKey,
+    algorithms: ['HS256']
+})
 
-var isRevokedCallback = function(req, payload, done){
-    var issuer = payload.iss;
-    var tokenId = payload.jti;
-  
-    data.getRevokedToken(issuer, tokenId, function(err, token){
-      if (err) { return done(err); }
-      return done(null, !!token);
-    });
-  };
+
 
 let users=[
     {
@@ -61,7 +47,7 @@ app.post('/api/login',(req,res)=>{
     {
         if(username == user.username && password == user.password)
         {
-          let token = jwt.sign({id : user.id,username: user.username },secretKey, {expiresIn :60*3});
+          let token = jwt.sign({id : user.id,username: user.username },secretKey, {expiresIn :180});
             res.json({
                 success: true,
                 err:null,
@@ -78,11 +64,10 @@ app.post('/api/login',(req,res)=>{
     });
 }); 
 
-app.get('/api/dashboard', jwtMW, (req,res) => {
-    console.log(req);
+app.get('/api/dashboard',jwtMW, (req,res) =>{
     res.json({
-        success: true,
-        myContent: 'Secret content that only logged in people can see',
+        success : true,
+        myContent: "Only logged in people can see"
     });
 });
 
@@ -93,37 +78,26 @@ app.get('/api/settings',jwtMW, (req,res) =>{
     });
 });
 
-app.get('/api/prices', jwtMW, (req,res) => {
-    console.log(req);
-    res.json({
-        success: true,
-        myContent: 'This is the price $4.00 !!!',
-    });
+app.get('/',(req,res) =>{
+    res.sendFile(path.join(__dirname,'index.html'));
 });
 
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.use(function (err, req, res, next) {
-
-    if(err.name === 'UnauthorizedError') {
-      if(err.inner.name == 'TokenExpiredError') {
-          console.log(err.inner.name);
-          axios.get('http://localhost:3000');
-          return;
-        }
+app.use(function(err,req,res,next){    
+    if(err.name === 'UnauthorizedError'){
+        // if(err.inner.name == "TokenExpiredError"){        
+        //     axios.get('http://localhost:3000');
+        //     return;
+        // }
         res.status(401).json({
-                success: false,
-                officialError: err,
-                err: 'Username or password is incorrect 2'
-        });
-    } else {
+            success: false,
+            officialError : err,
+            err: 'Username or password is incorrect 2'
+        });               
+    }else{
         next(err);
     }
-  });
+    
+})
 
-app.listen(PORT, () => {
-    console.log(`Serving on port ${PORT}`);
-});
+
+app.listen(PORT);
